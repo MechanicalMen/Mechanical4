72a8bd016900df516369aa918f1a1c0c5978655c
TastyBytes <mechanicalmen.tastybytes@gmail.com>
2017-08-08 21:59:47 +0200

    Bases classes added to help implementing IDisposable.

---
+++ source/Mechanical4.Core/Disposable.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Threading;
+
+namespace Mechanical4.Core
+{
+    /// <summary>
+    /// Helps implementing <see cref="IDisposable"/>.
+    /// </summary>
+    public static class Disposable
+    {
+        //// NOTE: This class doesn't directly help with unmanaged resources,
+        ////       but that's fine, since you likely don't need that. See:
+        ////       https://blog.stephencleary.com/2009/08/how-to-implement-idisposable-and.html
+
+        #region InvokeAtMostOnce
+
+        /// <summary>
+        /// Invokes the specified delegate at most once.
+        /// This class is thread-safe.
+        /// Use this class, when you need to implement <see cref="IDisposable"/>,
+        /// but are unable to use one of the base classes (since you already inherit from something else).
+        /// </summary>
+        public sealed class InvokeAtMostOnce
+        {
+            private Action action;
+
+            /// <summary>
+            /// Initializes a new instance of the <see cref="InvokeAtMostOnce"/> class.
+            /// </summary>
+            /// <param name="dlgt">The delegate to invoke (or <c>null</c>).</param>
+            public InvokeAtMostOnce( Action dlgt )
+            {
+                this.action = dlgt;
+            }
+
+            /// <summary>
+            /// Invokes the delegate on first call, does nothing afterwards.
+            /// </summary>
+            /// <returns><c>true</c> if the delegate was executed; otherwise, <c>false</c>.</returns>
+            public bool Invoke()
+            {
+                // - get the delegate from the field
+                // - set the field to null
+                // - try to invoke the delegate we got from the field
+                // - indicate what happened in return value
+                var tmp = Interlocked.Exchange(ref this.action, null);
+                if( tmp.NotNullReference() )
+                {
+                    tmp.Invoke();
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            /// <summary>
+            /// Gets a value indicating whether the next call to <see cref="Invoke"/> will execute the delegate.
+            /// </summary>
+            public bool NextInvokeExecutes => Interlocked.CompareExchange(ref this.action, null, null).NotNullReference();
+        }
+
+        #endregion
+
+        #region IDisposableObject
+
+        /// <summary>
+        /// A common interface implemented by the base classes here.
+        /// You likely won't need to use it directly.
+        /// </summary>
+        public interface IDisposableObject : IDisposable
+        {
+            /// <summary>
+            /// Gets a value indicating whether <see cref="IDisposable.Dispose"/> was called.
+            /// </summary>
+            bool IsDisposed { get; }
+        }
+
+        #endregion
+
+        #region NonBlockingBase
+
+        /// <summary>
+        /// Implements <see cref="IDisposable"/>.
+        /// When Dispose is called concurrently, the first call executes,
+        /// while subsequent calls do nothing and return immediately.
+        /// </summary>
+        public abstract class NonBlockingBase : IDisposableObject
+        {
+            private readonly InvokeAtMostOnce disposeAction;
+
+            /// <summary>
+            /// Initializes a new instance of the <see cref="NonBlockingBase"/> class.
+            /// </summary>
+            protected NonBlockingBase()
+            {
+                this.disposeAction = new InvokeAtMostOnce(this.OnDispose);
+            }
+
+            /// <summary>
+            /// Releases managed (IDisposable) resources.
+            /// </summary>
+            protected abstract void OnDispose();
+
+            /// <summary>
+            /// Disposes this instance.
+            /// </summary>
+            public void Dispose()
+            {
+                this.disposeAction.Invoke();
+            }
+
+            /// <summary>
+            /// Gets a value indicating whether <see cref="Dispose"/> was called.
+            /// </summary>
+            public bool IsDisposed => !this.disposeAction.NextInvokeExecutes;
+
+            /// <summary>
+            /// Throws an <see cref="ObjectDisposedException"/>, if this instance was already disposed.
+            /// </summary>
+            protected void ThrowIfDisposed()
+            {
+                if( this.IsDisposed )
+                    throw new ObjectDisposedException(null);
+            }
+        }
+
+        #endregion
+
+        #region BlockingBase
+
+        /// <summary>
+        /// Implements <see cref="IDisposable"/>.
+        /// When Dispose is called concurrently, the first call executes,
+        /// while subsequent calls block until the first one finishes.
+        /// </summary>
+        public abstract class BlockingBase : IDisposableObject
+        {
+            private readonly InvokeAtMostOnce disposeAction;
+            private readonly ManualResetEventSlim mre;
+
+            /// <summary>
+            /// Initializes a new instance of the <see cref="BlockingBase"/> class.
+            /// </summary>
+            protected BlockingBase()
+            {
+                this.disposeAction = new InvokeAtMostOnce(this.OnDispose);
+                this.mre = new ManualResetEventSlim(initialState: false); // nonsignaled
+            }
+
+            /// <summary>
+            /// Releases managed (IDisposable) resources.
+            /// </summary>
+            protected abstract void OnDispose();
+
+            /// <summary>
+            /// Disposes this instance.
+            /// </summary>
+            public void Dispose()
+            {
+                bool disposeInvoked = false; // stops compiler from nagging
+                try
+                {
+                    disposeInvoked = this.disposeAction.Invoke();
+                }
+                catch
+                {
+                    disposeInvoked = true; // the only way an exception could have been thrown
+                }
+                finally
+                {
+                    if( disposeInvoked )
+                        this.mre.Set();
+                    else
+                        this.mre.Wait();
+                }
+            }
+
+            /// <summary>
+            /// Gets a value indicating whether <see cref="Dispose"/> was called.
+            /// </summary>
+            public bool IsDisposed => !this.disposeAction.NextInvokeExecutes;
+
+            /// <summary>
+            /// Throws an <see cref="ObjectDisposedException"/>, if this instance was already disposed.
+            /// </summary>
+            protected void ThrowIfDisposed()
+            {
+                if( this.IsDisposed )
+                    throw new ObjectDisposedException(null);
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ source/Mechanical4.Tests/Core/DisposableTests.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Mechanical4.Core;
+using NUnit.Framework;
+
+namespace Mechanical4.Tests.Core
+{
+    [TestFixture]
+    public static class DisposableTests
+    {
+        #region Test types
+
+        private class DummyInvokeTest
+        {
+            public int Value { get; private set; }
+            public void Increase() => ++this.Value;
+        }
+
+        private interface IDisposableDummy : Disposable.IDisposableObject
+        {
+            DummyInvokeTest Dummy { get; }
+
+            void ThrowIfDisposed();
+        }
+
+        private class NonBlockingDummy : Disposable.NonBlockingBase, IDisposableDummy
+        {
+            public DummyInvokeTest Dummy { get; } = new DummyInvokeTest();
+
+            protected override void OnDispose()
+            {
+                this.Dummy.Increase();
+            }
+
+            public new void ThrowIfDisposed()
+            {
+                base.ThrowIfDisposed();
+            }
+        }
+
+        private class BlockingDummy : Disposable.BlockingBase, IDisposableDummy
+        {
+            public DummyInvokeTest Dummy { get; } = new DummyInvokeTest();
+
+            protected override void OnDispose()
+            {
+                this.Dummy.Increase();
+            }
+
+            public new void ThrowIfDisposed()
+            {
+                base.ThrowIfDisposed();
+            }
+        }
+
+        private class NonBlockingLongDispose : Disposable.NonBlockingBase
+        {
+            protected override void OnDispose()
+            {
+                Thread.Sleep(TimeSpan.FromMilliseconds(300));
+            }
+        }
+
+        private class BlockingLongDispose : Disposable.BlockingBase
+        {
+            protected override void OnDispose()
+            {
+                Thread.Sleep(TimeSpan.FromMilliseconds(300));
+            }
+        }
+
+        #endregion
+
+        [Test]
+        public static void InvokeAtMostOnce_NullDelegate()
+        {
+            var invoker = new Disposable.InvokeAtMostOnce(null);
+            Assert.False(invoker.NextInvokeExecutes);
+            Assert.False(invoker.Invoke());
+            Assert.False(invoker.NextInvokeExecutes);
+            Assert.False(invoker.Invoke());
+        }
+
+        [Test]
+        public static void InvokeAtMostOnce_ExactlyOneInvoke()
+        {
+            var dummy = new DummyInvokeTest();
+            var invoker = new Disposable.InvokeAtMostOnce(dummy.Increase);
+
+            // initially not invoked
+            Assert.True(invoker.NextInvokeExecutes);
+            Assert.AreEqual(0, dummy.Value);
+
+            // first invoke executes delegate
+            Assert.True(invoker.Invoke());
+            Assert.AreEqual(1, dummy.Value);
+            Assert.False(invoker.NextInvokeExecutes);
+
+            // subsequent invokes have no effect
+            foreach( var i in Enumerable.Range(0, 3) )
+            {
+                Assert.False(invoker.Invoke());
+                Assert.AreEqual(1, dummy.Value);
+                Assert.False(invoker.NextInvokeExecutes);
+            }
+        }
+
+        private static void DisposableBase_ExactlyOneDispose( IDisposableDummy testObj )
+        {
+            Assert.NotNull(testObj);
+
+            // initially not disposed
+            Assert.False(testObj.IsDisposed);
+            Assert.AreEqual(0, testObj.Dummy.Value);
+
+            // first Dispose executes
+            testObj.Dispose();
+            Assert.True(testObj.IsDisposed);
+            Assert.AreEqual(1, testObj.Dummy.Value);
+
+            // subsequent calls have no effect
+            foreach( var i in Enumerable.Range(0, 3) )
+            {
+                testObj.Dispose();
+                Assert.True(testObj.IsDisposed);
+                Assert.AreEqual(1, testObj.Dummy.Value);
+            }
+        }
+
+        private static void DisposableBase_ThrowIfDisposed( IDisposableDummy testObj )
+        {
+            Assert.NotNull(testObj);
+            Assert.False(testObj.IsDisposed);
+
+            testObj.ThrowIfDisposed();
+            testObj.Dispose();
+            Assert.Throws<ObjectDisposedException>(() => testObj.ThrowIfDisposed());
+        }
+
+        private static void DisposableBase_ConcurrentDispose( Disposable.IDisposableObject testObj, bool callsExpectedToFinishAboutTheSameTime )
+        {
+            Assert.NotNull(testObj);
+            Assert.False(testObj.IsDisposed);
+
+            // start multiple Dispose calls: first one, than others concurrently
+            var stopwatch = Stopwatch.StartNew();
+            var tasks = Enumerable.Range(0, 5)
+                .Select(index =>
+                {
+                    var t = Task.Run(() =>
+                    {
+                        testObj.Dispose();
+                        return (Index: index, FinishedTimestamp: stopwatch.Elapsed);
+                    });
+                    if( index == 0 )
+                        Thread.Sleep(TimeSpan.FromMilliseconds(100));
+                    return t;
+                })
+                .ToArray();
+
+            // wait for all Dispose calls to finish
+            Task.WaitAll(tasks);
+
+            // check results
+            var firstDisposeTimestamp = tasks[0].Result.FinishedTimestamp;
+            bool tasksFinishedCloseToEachOther = tasks.Select(t => Math.Abs((t.Result.FinishedTimestamp - firstDisposeTimestamp).Ticks)).Max() < TimeSpan.FromMilliseconds(50).Ticks;
+            Assert.AreEqual(callsExpectedToFinishAboutTheSameTime, tasksFinishedCloseToEachOther);
+        }
+
+        [Test]
+        public static void NonBlockingBaseTests()
+        {
+            DisposableBase_ExactlyOneDispose(new NonBlockingDummy());
+            DisposableBase_ThrowIfDisposed(new NonBlockingDummy());
+            DisposableBase_ConcurrentDispose(new NonBlockingLongDispose(), callsExpectedToFinishAboutTheSameTime: false);
+        }
+
+        [Test]
+        public static void BlockingBaseTests()
+        {
+            DisposableBase_ExactlyOneDispose(new BlockingDummy());
+            DisposableBase_ThrowIfDisposed(new BlockingDummy());
+            DisposableBase_ConcurrentDispose(new BlockingLongDispose(), callsExpectedToFinishAboutTheSameTime: true);
+        }
+    }
+}
